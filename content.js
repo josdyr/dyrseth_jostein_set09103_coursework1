30a532444cd9d2cbf7d51c973f64b725fa11b687
Jostein Dyrseth <josdyr@Josteins-MacBook-Pro.local>
2017-10-12 15:50:54 +0100

    some initial file creation

---
+++ content.js
@@ -0,0 +1,11 @@
+data = {
+  "Movie_0000": {
+    "title":"Titanic",
+    "image":"/path/to/file.png",
+    "cover_image":"/path/to/file.png",
+    "description":"desc...",
+    "rating": 10,
+    "tags": ["a", "lot", "of", "tags"],
+    "trailer": "/path/to/file.png"
+  }
+}
