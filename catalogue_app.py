65c4bde23cc1c40c5fccb019e4ca1e952990a39c
Jostein Dyrseth <josdyr@josdyr-mac.local>
2017-10-17 17:51:01 +0100

    some fixes

--- catalogue_app.py
+++ catalogue_app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+import json
+import pprint
 
 app = Flask(__name__)
 
 
+with open('templates/index.html') as data_file:
+    data = json.load(data_file)
+pprint(data)
+
+
 @app.route("/")
+@app.route("/index")
 def index():
-    return render_template("template.html")
+    names = ['simon', 'thomas', 'lee', 'jamie', 'sylvester']
+    return render_template("templates/index.html", title="Home|", names=names)
 
 
-app.run(debug=True, port=8000, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
--- content.js
+++ content.js
@@ -1,11 +1,31 @@
-data = {
-  "Movie_0000": {
-    "title":"Titanic",
-    "image":"/path/to/file.png",
-    "cover_image":"/path/to/file.png",
-    "description":"desc...",
-    "rating": 10,
-    "tags": ["a", "lot", "of", "tags"],
-    "trailer": "/path/to/file.png"
-  }
+{
+	"data": {
+		"Movie_0000": {
+			"title": "Titanic",
+			"image": "/path/to/file.png",
+			"cover_image": "/path/to/file.png",
+			"description": "desc...",
+			"rating": 10,
+			"tags": ["a", "lot", "of", "tags"],
+			"trailer": "/path/to/file.png"
+		},
+		"Movie_0001": {
+			"title": "Harry Potter",
+			"image": "/path/to/file.png",
+			"cover_image": "/path/to/file.png",
+			"description": "desc...",
+			"rating": 10,
+			"tags": ["a", "lot", "of", "tags"],
+			"trailer": "/path/to/file.png"
+		},
+		"Movie_0002": {
+			"title": "Trainspotting",
+			"image": "/path/to/file.png",
+			"cover_image": "/path/to/file.png",
+			"description": "desc...",
+			"rating": 10,
+			"tags": ["a", "lot", "of", "tags"],
+			"trailer": "/path/to/file.png"
+		}
+	}
 }
