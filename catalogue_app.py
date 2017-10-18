2e068e266c9e8a646d471f7949abe465d837e53c
Jostein Dyrseth <josdyr@josdyr-mac.local>
2017-10-18 16:14:50 +0100

    Added data to static folder, importing the data to the flask app.

--- catalogue_app.py
+++ catalogue_app.py
@@ -1,21 +1,30 @@
 from flask import Flask, render_template
 import json
-import pprint
+# import data_Hardware
+# import pprint
+# import http.client
+
 
 app = Flask(__name__)
 
 
-with open('templates/index.html') as data_file:
-    data = json.load(data_file)
-pprint(data)
+with open('static/data/data_4_movies.json') as in_file:
+    movie_dict = json.load(in_file)
+    in_file.close()
+    # import pdb
+    # pdb.set_trace()
+
+img_path = 'http://image.tmdb.org/t/p/w342'
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    names = ['simon', 'thomas', 'lee', 'jamie', 'sylvester']
-    return render_template("templates/index.html", title="Home|", names=names)
+    return render_template("index.html",
+                           movies=movie_dict['results'],
+                           img_path=img_path
+                           )
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
